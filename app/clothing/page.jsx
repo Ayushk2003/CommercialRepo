28dc9205fcbacde3e84ab168d87ff80894dc2b69
Ayushk2003 <ayushkushwaha182003@gmail.com>
2026-06-27 17:33:10 +0530

    coming soon refrence corrected

--- app/clothing/page.jsx
+++ app/clothing/page.jsx
@@ -1,11 +1,9 @@
 import ComingSoon from "../components/ComingSoon";
-// import ClothingSoon from "../components/ClothingComingSoon";
 
 export default function ClothingPage() {
   return (
     <>
       <ComingSoon />
-      {/* <ClothingSoon /> */}
     </>
   );
 }
